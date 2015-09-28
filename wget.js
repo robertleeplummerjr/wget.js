c01bfe4dcfe9f5a5de7b9bdfebe1b09cc2e37e08
Robert Plummer <robertleeplummerjr@gmail.com>
2015-09-28 08:53:42 -0400

    initual

---
+++ wget.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function wget(urls, fn) {
+	urls = urls.push !== undefined ? urls : [urls];
+
+	var results = [],
+		lookup = {},
+		complete = 0,
+		total = urls.length;
+
+	urls.forEach(function(url) {
+		var i = lookup[url] = results.length,
+			request = new XMLHttpRequest();
+		results.push(null);
+		request.open('GET', url, true);
+		request.onload = function () {
+			if (request.status < 200 && request.status > 400) return;
+			results[i] = request.responseText;
+			complete++;
+			if (complete === total && fn !== undefined) fn(results);
+		};
+		request.send();
+	});
+}
